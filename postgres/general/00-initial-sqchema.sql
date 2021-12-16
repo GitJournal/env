64836009ba81725642b8c9efe92d933a555bc017
Vishesh Handa <me@vhanda.in>
2021-12-16 15:59:43 +0100

    Postgres

---
+++ postgres/general/00-initial-sqchema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE json_data (
+	id varchar NOT NULL PRIMARY KEY,
+	`data` json NOT NULL
+);
+
+-- INSERT INTO json_data (id, data)
+-- VALUES('support_pro_emails', '{"count": 10}') on conflict(id)
+-- do update set data = '{"count": 10}';
