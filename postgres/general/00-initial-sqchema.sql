e4370f3158ff21dff6162ff3f7c7a9db92674fab
Vishesh Handa <me@vhanda.in>
2021-12-27 17:53:07 +0100

    Update db

--- postgres/general/00-initial-sqchema.sql
+++ postgres/general/00-initial-sqchema.sql
@@ -6,3 +6,7 @@ CREATE TABLE json_data (
 -- INSERT INTO json_data (id, data)
 -- VALUES('support_pro_emails', '{"count": 10}') on conflict(id)
 -- do update set data = '{"count": 10}';
+
+CREATE TABLE tweets_read (
+	id bigint NOT NULL PRIMARY KEY
+);
